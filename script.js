80a67597644f5a1ae3d93ce25d9988c50b9e35b4
Yechiel K <chilikkal@gmail.com>
2016-12-11 00:53:27 -0500

    practiced $.each

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
-
-
+  var names = ["Carleton", "Avi", "Azat", "Ann"]
+  $.each(names, function(index, name) {
+    console.log("Hey " + (index + 1) + ", " + name);
+  })
 });
